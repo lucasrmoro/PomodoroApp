1b1354ccf707fb993ed21a0e626265062a374049
Lucas <lucas.rmoro1@gmail.com>
2021-05-30 18:41:01 -0300

    Keep relation between list task view and list task adapter

--- app/src/main/java/br/com/lucas/pomodoroapp/ui/ListTaskActivity.kt
+++ app/src/main/java/br/com/lucas/pomodoroapp/ui/ListTaskActivity.kt
@@ -1,10 +1,13 @@
 package br.com.lucas.pomodoroapp.ui
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.lucas.pomodoroapp.R
+import br.com.lucas.pomodoroapp.database.Task
 import br.com.lucas.pomodoroapp.databinding.ActivityListTaskBinding
 
 class ListTaskActivity : AppCompatActivity() {
@@ -18,7 +21,12 @@ class ListTaskActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         binding.fab.setOnClickListener { view ->
-           EditTaskActivity.launch(this)
+            EditTaskActivity.launch(this)
         }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = ListTaskAdapter()
+
+        val tasks = (1 .. 100).map { Task(it, "Test$it", it * 100)}
+        (binding.recyclerView.adapter as ListTaskAdapter).addTask(tasks)
     }
 }
---
+++ app/src/main/java/br/com/lucas/pomodoroapp/ui/ListTaskAdapter.kt
@@ -0,0 +1,45 @@
+package br.com.lucas.pomodoroapp.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import br.com.lucas.pomodoroapp.database.Task
+import br.com.lucas.pomodoroapp.databinding.ListTaskItemBinding
+
+class ListTaskAdapter : Adapter<ListTaskAdapter.TaskViewHolder>() {
+
+    private val tasks = mutableListOf<Task>()
+
+    fun addTask(tasks: List<Task>) {
+        this.tasks.addAll(tasks)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        return TaskViewHolder(
+            ListTaskItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = tasks[position]
+        holder.bind(task)
+    }
+
+    override fun getItemCount(): Int {
+        return tasks.size
+    }
+
+    class TaskViewHolder(private val binding: ListTaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task) {
+            binding.itemTaskName.text = task.taskName
+            binding.itemTaskTime.text = task.taskMinutes.toString()
+        }
+    }
+}
