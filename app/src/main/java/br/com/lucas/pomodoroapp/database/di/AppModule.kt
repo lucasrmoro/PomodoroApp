1e51e15227bf5f3549fd6d650bac23498da07b0b
lucasrmoro <lucas.rmoro1@gmail.com>
2022-01-04 15:05:17 -0300

    Creating the App Module file

---
+++ app/src/main/java/br/com/lucas/pomodoroapp/database/di/AppModule.kt
@@ -0,0 +1,27 @@
+package br.com.lucas.pomodoroapp.database.di
+
+import android.content.Context
+import androidx.room.Room
+import br.com.lucas.pomodoroapp.database.AppDataBase
+import br.com.lucas.pomodoroapp.database.TaskDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTaskDao(appDataBase: AppDataBase): TaskDao = appDataBase.taskDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDataBase =
+        Room.databaseBuilder(context, AppDataBase::class.java, "pomodoro-db")
+            .build()
+}
