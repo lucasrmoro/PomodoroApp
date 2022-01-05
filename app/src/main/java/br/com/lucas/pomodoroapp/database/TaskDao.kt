87175c9eca427f008d64c2362a626fbb70976baf
lucasrmoro <lucas.rmoro1@gmail.com>
2022-01-04 23:35:38 -0300

    Removing suspend from getAll function on Dao and Repository

--- app/src/main/java/br/com/lucas/pomodoroapp/database/TaskDao.kt
+++ app/src/main/java/br/com/lucas/pomodoroapp/database/TaskDao.kt
@@ -8,7 +8,7 @@ interface TaskDao {
     @Insert
     suspend fun insertTask(task: Task)
     @Query("SELECT * FROM task")
-    suspend fun getAll() : Flow<List<Task>>
+    fun getAll() : Flow<List<Task>>
     @Update
     suspend fun updateTask(task: Task)
     @Delete
--- app/src/main/java/br/com/lucas/pomodoroapp/database/TaskRepository.kt
+++ app/src/main/java/br/com/lucas/pomodoroapp/database/TaskRepository.kt
@@ -10,6 +10,6 @@ class TaskRepository @Inject constructor(private val taskDao: TaskDao) {
     suspend fun insertTask(task: Task) = taskDao.insertTask(task)
     suspend fun updateTask(task: Task) = taskDao.updateTask(task)
     suspend fun deleteTask(task: Task) = taskDao.deleteTask(task)
-    suspend fun getAllTasks(): Flow<List<Task>> = taskDao.getAll().flowOn(Dispatchers.IO)
+    fun getAllTasks(): Flow<List<Task>> = taskDao.getAll().flowOn(Dispatchers.IO)
         .conflate()
 }
