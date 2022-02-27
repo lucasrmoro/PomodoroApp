857426f5909ac1ed7802411700a07509d10e2479
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-27 14:36:54 -0300

    Creating a Timber DebugTree to show the logs

--- app/src/main/java/br/com/lucas/pomodoroapp/PomodoroApplication.kt
+++ app/src/main/java/br/com/lucas/pomodoroapp/PomodoroApplication.kt
@@ -2,7 +2,14 @@ package br.com.lucas.pomodoroapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class PomodoroApplication: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
