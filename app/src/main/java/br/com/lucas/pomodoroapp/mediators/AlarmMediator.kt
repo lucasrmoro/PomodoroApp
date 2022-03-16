2ac58bf4fa569ad539934340b4e903d75e9f7149
lucasrmoro <lucas.rmoro1@gmail.com>
2022-03-15 23:02:46 -0300

    Creating an Alarm Mediator to enable the task timer and save it

---
+++ app/src/main/java/br/com/lucas/pomodoroapp/mediators/AlarmMediator.kt
@@ -0,0 +1,28 @@
+package br.com.lucas.pomodoroapp.mediators
+
+import br.com.lucas.pomodoroapp.helpers.AlarmManagerHelper
+import br.com.lucas.pomodoroapp.helpers.PreferencesHelper
+import javax.inject.Inject
+
+class AlarmMediator @Inject constructor(
+    private val alarmManagerHelper: AlarmManagerHelper,
+    private val preferencesHelper: PreferencesHelper,
+) {
+    fun syncTaskTimer(
+        isTimerEnabled: Boolean,
+        taskId: Int,
+        alarmTime: Int,
+    ) {
+        if (isTimerEnabled) {
+            alarmManagerHelper.setAlarm(alarmTime){
+                preferencesHelper.saveTaskTimer(taskId)
+            }
+        } else {
+            preferencesHelper.deleteTaskTimer()
+            alarmManagerHelper.cancelAlarm()
+        }
+    }
+
+    val taskTimerEnabled: Int
+        get() = preferencesHelper.taskTimerEnabled
+}
