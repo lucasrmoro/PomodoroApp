12a8f6cc558dcc7e4af9b5ca942e241d15b15079
Lucas Moro <lucas.rmoro1@gmail.com>
2022-03-22 21:48:01 -0300

    Saving the values of Pomodoro steps in the database

--- app/src/main/java/br/com/lucas/pomodoroapp/ui/editTaskScreen/EditTaskActivity.kt
+++ app/src/main/java/br/com/lucas/pomodoroapp/ui/editTaskScreen/EditTaskActivity.kt
@@ -1,9 +1,7 @@
 package br.com.lucas.pomodoroapp.ui.editTaskScreen
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -12,14 +10,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.observe
 import br.com.lucas.pomodoroapp.R
-import br.com.lucas.pomodoroapp.core.extensions.convertMinutesToHour
-import br.com.lucas.pomodoroapp.core.extensions.getColorResCompat
+import br.com.lucas.pomodoroapp.core.extensions.putInTheRespectiveViews
 import br.com.lucas.pomodoroapp.core.extensions.toast
+import br.com.lucas.pomodoroapp.database.model.PomodoroDurations
 import br.com.lucas.pomodoroapp.database.model.Task
 import br.com.lucas.pomodoroapp.databinding.ActivityEditTaskBinding
 import br.com.lucas.pomodoroapp.helpers.AlertDialogHelper
-import com.google.android.material.timepicker.MaterialTimePicker.Builder
-import com.google.android.material.timepicker.TimeFormat
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -27,7 +23,6 @@ import timber.log.Timber
 class EditTaskActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityEditTaskBinding
-
     private val viewModel by viewModels<EditTaskViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,31 +33,58 @@ class EditTaskActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val task: Task? = intent.getParcelableExtra(TASK_NAME_KEY) as? Task
+        var durations = PomodoroDurations()
 
         if (task != null) {
             viewModel.setup(task)
+            durations = task.pomodoroDurations
+            binding.edtTaskName.setText(task.taskName)
             binding.toolbar.title = getString(R.string.edit_task_toolbar_label)
-            binding.editTaskName.setText("${viewModel.task?.taskName}")
-            binding.editPomodoroTimer.text =
-                "${viewModel.task?.taskMinutes?.convertMinutesToHour()}"
         }
 
-        binding.editPomodoroTimer.setOnClickListener() {
-            showTimePicker()
+        durations.pomodoroTime.putInTheRespectiveViews(
+            binding.sliderPomodoroTime, binding.edtPomodoroTime)
+
+        durations.shortBreakTime.putInTheRespectiveViews(
+            binding.sliderShortBreakTime, binding.edtShortBreakTime)
+
+        durations.longBreakTime.putInTheRespectiveViews(
+            binding.sliderLongBreakTime, binding.edtLongBreakTime)
+
+        durations.numberOfCycles.putInTheRespectiveViews(
+            binding.sliderPomodoroTimersCycles, binding.edtPomodoroTimersCycles)
+
+        binding.sliderPomodoroTime.addOnChangeListener { _, value, _ ->
+            binding.edtPomodoroTime.text = value.toInt().toString()
         }
 
-        binding.editPomodoroTimer.doAfterTextChanged {
-            Timber.d("Pomodoro time: $it")
+        binding.sliderShortBreakTime.addOnChangeListener { _, value, _ ->
+            binding.edtShortBreakTime.text = value.toInt().toString()
         }
 
-        binding.editTaskName.doAfterTextChanged {
+        binding.sliderLongBreakTime.addOnChangeListener { _, value, _ ->
+            binding.edtLongBreakTime.text = value.toInt().toString()
+        }
+
+        binding.sliderPomodoroTimersCycles.addOnChangeListener { _, value, _ ->
+            binding.edtPomodoroTimersCycles.text = value.toInt().toString()
+        }
+
+        binding.edtTaskName.doAfterTextChanged {
             viewModel.checkTaskNameIsValid(it.toString())
             Timber.d("Task name: $it")
         }
 
         binding.fabSave.setOnClickListener {
+            val pomodoroDurations = PomodoroDurations(
+                binding.sliderPomodoroTime.value.toInt(),
+                binding.sliderShortBreakTime.value.toInt(),
+                binding.sliderLongBreakTime.value.toInt(),
+                binding.sliderPomodoroTimersCycles.value.toInt(),
+            )
             viewModel.onSaveEvent(
-                taskName = binding.editTaskName.text.toString(),
+                taskName = binding.edtTaskName.text.toString(),
+                pomodoroDurations = pomodoroDurations,
                 toastOfSuccessUpdate = { toast(R.string.successfully_changed) },
                 toastOfSuccessAdd = { toast(R.string.successfully_saved) },
                 toastOfFail = { toast(R.string.fill_all_required_fields) },
@@ -74,27 +96,10 @@ class EditTaskActivity : AppCompatActivity() {
             toast(R.string.feature_isnt_implemented)
         }
 
-        viewModel.isTaskNameValid.observe(this) {
-            if (it == true) {
-                binding.taskName.setTextColor(this.getColorResCompat(android.R.attr.textColorPrimary))
-            } else {
-                binding.taskName.setTextColor(Color.RED)
-            }
+        viewModel.isTaskNameValid.observe(this) { isNameValid ->
+            binding.edtTaskNameLayout.error =
+                if (!isNameValid) getString(R.string.you_have_to_name_the_task) else null
         }
-
-        viewModel.isPomodoroTimerValid.observe(this) {
-            if (it == false) {
-                toast(R.string.select_valid_time, 3500)
-                binding.pomodoroTimer.setTextColor(Color.RED)
-            } else {
-                binding.pomodoroTimer.setTextColor(this.getColorResCompat(android.R.attr.textColorPrimary))
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.editTaskName.requestFocus()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -108,7 +113,7 @@ class EditTaskActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_delete_action) {
-            if(viewModel.isTaskEnabled){
+            if (viewModel.isTaskEnabled) {
                 toast(R.string.can_not_delete_task_with_active_timer)
             } else {
                 deleteTask()
@@ -136,21 +141,6 @@ class EditTaskActivity : AppCompatActivity() {
             },
             negativeButtonMessage = R.string.cancel
         )
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun showTimePicker() {
-        val picker = Builder()
-            .setTimeFormat(TimeFormat.CLOCK_24H)
-            .setHour(0)
-            .setMinute(25)
-            .setTitleText(R.string.select_pomodoro_time)
-            .build()
-        picker.show(supportFragmentManager, "Test")
-        picker.addOnPositiveButtonClickListener {
-            viewModel.checkTaskTimeIsValid(picker.hour, picker.minute)
-            binding.editPomodoroTimer.text = viewModel.total.convertMinutesToHour()
-        }
     }
 
     companion object {
--- app/src/main/java/br/com/lucas/pomodoroapp/ui/editTaskScreen/EditTaskViewModel.kt
+++ app/src/main/java/br/com/lucas/pomodoroapp/ui/editTaskScreen/EditTaskViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.lucas.pomodoroapp.database.model.Task
 import br.com.lucas.pomodoroapp.database.TaskRepository
+import br.com.lucas.pomodoroapp.database.model.PomodoroDurations
 import br.com.lucas.pomodoroapp.helpers.PreferencesHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,15 +19,10 @@ class EditTaskViewModel @Inject constructor(
     private val preferencesHelper: PreferencesHelper,
 ) : ViewModel() {
 
-    var total: Int = 25
-        private set
-
     val isTaskEnabled: Boolean
         get() = task?.uid == preferencesHelper.taskTimerEnabled
 
-    val isPomodoroTimerValid = MutableLiveData<Boolean>()
     val isTaskNameValid = MutableLiveData<Boolean>()
-    private val HOUR_ON_MINUTES = 60
 
     var isEditMode = false
         private set
@@ -37,7 +33,6 @@ class EditTaskViewModel @Inject constructor(
     fun setup(task: Task) {
         this.task = task
         this.isEditMode = true
-        total = task.taskMinutes
     }
 
     fun delete(closeScreen: EditTaskCallback = {}, toastOfSuccess: EditTaskCallback = {}) {
@@ -53,43 +48,33 @@ class EditTaskViewModel @Inject constructor(
         isTaskNameValid.value = taskName.length >= 3
     }
 
-    fun checkTaskTimeIsValid(hour: Int, minute: Int) {
-        val hoursInMinutes = hour * HOUR_ON_MINUTES
-        total = hoursInMinutes + minute
-        checkTotalTime()
-    }
-
-    private fun checkTotalTime() {
-        isPomodoroTimerValid.value = total in 25..60
-    }
-
     fun onSaveEvent(
         taskName: String,
+        pomodoroDurations: PomodoroDurations,
         closeScreen: EditTaskCallback = {},
         toastOfSuccessUpdate: EditTaskCallback = {},
         toastOfSuccessAdd: EditTaskCallback = {},
-        toastOfFail: EditTaskCallback = {}
+        toastOfFail: EditTaskCallback = {},
     ) {
-        if (task == null) {
-            saveNewTask(taskName, toastOfSuccessAdd, toastOfFail, closeScreen)
+        if (this.task == null) {
+            saveNewTask(taskName, pomodoroDurations, toastOfSuccessAdd, toastOfFail, closeScreen)
         } else {
-            task!!.taskName = taskName
-            task!!.taskMinutes = total
-            checkTaskNameIsValid(task!!.taskName)
-            checkTotalTime()
-            saveSameTask(task!!, closeScreen, toastOfSuccessUpdate, toastOfFail)
+            saveSameTask(taskName, pomodoroDurations, closeScreen, toastOfSuccessUpdate, toastOfFail)
         }
     }
 
     private fun saveSameTask(
-        task: Task,
+        taskName: String,
+        pomodoroDurations: PomodoroDurations,
         closeScreen: EditTaskCallback,
         toastOfSuccessUpdate: EditTaskCallback,
-        toastOfFail: EditTaskCallback
+        toastOfFail: EditTaskCallback,
     ) {
-        if (isPomodoroTimerValid.value == true && isTaskNameValid.value == true) {
+        checkTaskNameIsValid(task!!.taskName)
+        if (isTaskNameValid.value == true) {
+            this.task = task!!.copy(taskName = taskName, pomodoroDurations = pomodoroDurations)
             viewModelScope.launch {
-                repository.updateTask(task)
+                repository.updateTask(task!!)
                 toastOfSuccessUpdate()
                 closeScreen()
             }
@@ -100,17 +85,18 @@ class EditTaskViewModel @Inject constructor(
 
     private fun saveNewTask(
         taskName: String,
+        pomodoroDurations: PomodoroDurations,
         toastOfSuccessAdd: EditTaskCallback,
         toastOfFail: EditTaskCallback,
-        closeScreen: EditTaskCallback
+        closeScreen: EditTaskCallback,
     ) {
-        if (isPomodoroTimerValid.value == true && isTaskNameValid.value == true) {
+        if (isTaskNameValid.value == true) {
             viewModelScope.launch {
                 repository.insertTask(
                     Task(
+                        uid = 0,
                         taskName = taskName,
-                        taskMinutes = total,
-                        uid = 0
+                        pomodoroDurations = pomodoroDurations
                     )
                 )
                 toastOfSuccessAdd()
